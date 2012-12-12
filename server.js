bcfcaf0f3915c6185e9df2e14f9bb160f0f3b3e6
David Herron <david@davidherron.com>
2012-12-11 22:36:39 -0800

    Add GPL Version 3 license

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 /**
-* Simple webserver with logging. By default, serves whatever files are
-* reachable from the directory where node is running.
-*/
+ * This file is part of AkashaCMS (http://akashacms.com/).
+ *
+ *   AkashaCMS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   AkashaCMS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with AkashaCMS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Simple webserver with logging. By default, serves whatever files are
+ * reachable from the directory where node is running.
+ */
 var fs = require('fs'),
 antinode = require('./antinode/lib/antinode'),
 sys = require('sys');
