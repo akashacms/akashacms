90a733bc23c8e9ae8791c6da1e2bbb064ed7a7d0
David Herron <david@davidherron.com>
2012-12-29 19:36:22 -0800

    Switch from GPL2 to Apache2 license

--- cli.js
+++ cli.js
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
 
+/**
+ *
+ * Copyright 2012 David Herron
+ * 
+ * This file is part of AkashaCMS (http://akashacms.com/).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ **/
+
 // akasha init dirName -- initialize new directory
 // akasha build
 // akasha serve
 
 var util    = require('util');
 var spawn = require('child_process').spawn;
+var exec  = require('child_process').exec;
 var http = require('http');
 var program = require('commander');
 var akasha = require('akashacms');
@@ -19,8 +39,18 @@ program
 
 program
     .command('init <dirName>')
-    .description('initialize a akashacms site')
+    .description('initialize an akashacms site')
     .action(function(dirName){
+        /*var git = exec(
+                'git clone git://github.com/robogeek/akashacms-example.git' + dirName,
+                {env: process.env, stdio: 'inherit'},
+                function (error, stdout, stderr) {
+                    console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });*/
         var git = spawn('git',
               [ 'clone', 'git://github.com/robogeek/akashacms-example.git', dirName],
               {env: process.env, stdio: 'inherit'});
