bebe81c02a824f96987c97d05337052e2a409fd3
David Herron <david@davidherron.com>
2013-09-07 17:11:27 -0700

    Fix path for layouts

--- builtin/index.js
+++ builtin/index.js
@@ -2,5 +2,5 @@ var path = require('path');
 
 module.exports.config = function(akashacms, config) {
     config.root_partials.push(path.join(__dirname, 'partials'));
-    config.root_layouts.push(path.join(__dirname, 'layouts'));
+    config.root_layouts.push(path.join(__dirname, 'layout'));
 }
